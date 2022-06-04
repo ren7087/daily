f1b95cb1778e483ea2955fb1a1f3f301565131ca
User <user@MacBook-Air.local>
2022-06-04 17:51:16 +0900

    ボタンの位置変更

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,10 +13,10 @@
             <a href="/post/add">日報登録</a>
         </div>
         <div class="button09">
-            <a href="/post/index2">日報表示カレンダー形式</a>
+            <a href="/post/page">日報表示エクセル形式</a>
         </div>
         <div class="button09">
-            <a href="/post/page">日報表示エクセル形式</a>
+            <a href="/post/index2">日報表示カレンダー形式</a>
         </div>
     </div>
 </x-app>
