37a926d4dbd5e6360ca069ce0e2d926de7cb872d
User <user@MacBook-Air.local>
2022-06-18 13:12:19 +0900

    営業結果登録作成

--- resources/views/nippouresult.blade.php
+++ resources/views/nippouresult.blade.php
@@ -1,60 +1,145 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/dropdown.css') }}">
+    <title>お問い合わせ</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
-    <div class="cp_ipselect" style="margin-top: 340px">
-        <ul class="cp_sl08">
-            <li>
-            <input class="cp_sl08_close" type="radio" name="awesomeness" id="close" value=""/>
-            <span class="cp_sl08_label cp_sl08_placeholder">選択してください</span>
-            </li>
-            <li class="cp_sl08_items">
-            <input class="cp_sl08_expand" type="radio" name="option08" id="opener"/>
-            <label class="cp_sl08_closeLabel" for="close"></label>
-                <ul class="cp_sl08_options">
-                    <li>
-                        <input class="cp_sl08_input" type="radio" name="option08" id="8"/>
-                        <label class="cp_sl08_label" for="8">8:00</label>
-                    </li>
-                    <li>
-                        <input class="cp_sl08_input" type="radio" name="option08" id="10"/>
-                        <label class="cp_sl08_label" for="10">10:00</label>
-                    </li>
-                    <li>
-                        <input class="cp_sl08_input" type="radio" name="option08" id="12"/>
-                        <label class="cp_sl08_label" for="12">12:00</label>
-                    </li>
-                    <li>
-                        <input class="cp_sl08_input" type="radio" name="option08" id="14"/>
-                        <label class="cp_sl08_label" for="14">14:00</label>
-                    </li>
-                    <li>
-                        <input class="cp_sl08_input" type="radio" name="option08" id="16"/>
-                        <label class="cp_sl08_label" for="16">16:00</label>
-                    </li>
-                    <li>
-                        <input class="cp_sl08_input" type="radio" name="option08" id="18"/>
-                        <label class="cp_sl08_label" for="18">18:00</label>
-                    </li>
-                    <li>
-                        <input class="cp_sl08_input" type="radio" name="option08" id="20"/>
-                        <label class="cp_sl08_label" for="20">20:00</label>
-                    </li>
-                    <li>
-                        <input class="cp_sl08_input" type="radio" name="option08" id="22"/>
-                        <label class="cp_sl08_label" for="22">22:00</label>
-                    </li>
-                </ul>
-            <label class="cp_sl08_expandLabel" for="opener"></label>
-            </li>
-        </ul>
+    <div class="container">
+        <h1 class="text-center mt-2 mb-5">お問い合わせ</h1>
+        <div class="container">
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">お名前（全角10文字以内）<span class="badge badge-danger ml-1">必須</span></p>
+                <div class="col-sm-8">
+                    {{ Form::text('name', null, ['class' => 'form-control']) }}
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">メールアドレス<span class="badge badge-danger ml-1">必須</span></p>
+                <div class="col-sm-8">
+                    {{ Form::text('email', null, ['class' => 'form-control']) }}
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">電話番号</p>
+                <div class="col-sm-8">
+                    {{ Form::text('tel', null, ['class' => 'form-control']) }}
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">性別<span class="badge badge-danger ml-1">必須</span></p>
+                <div class="col-sm-8">
+                    <label>{{ Form::radio('gender', "男性") }}男性</label>
+                    <label>{{ Form::radio('gender', "女性") }}女性</label>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">選択（複数選択可）<span class="badge badge-danger ml-1">必須</span></p>
+                <div class="col-sm-8">
+                    <label>{{ Form::checkbox('checkbox', "選択肢１") }}選択肢１</label>
+                    <label>{{ Form::checkbox('checkbox', "選択肢２") }}選択肢２</label>
+                    <label>{{ Form::checkbox('checkbox', "選択肢３") }}選択肢３</label>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">お問い合わせ内容<span class="badge badge-danger ml-1">必須</span></p>
+                <div class="col-sm-8">
+                    {{ Form::textarea('contents', null, ['class' => 'form-control']) }}
+                </div>
+            </div>
+
+            <div class="text-center">
+                <button type="button" class="btn btn-primary form-btn" data-toggle="modal" data-target="#exampleModalCenter">
+                    確認画面へ
+                </button>
+            </div>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">確認画面</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+                            {{ csrf_field() }}
+                            <div class="form-group row">
+                                <p class="col-sm-4 col-form-label">お名前（全角10文字以内）<span class="badge badge-danger ml-1">必須</span></p>
+                                <div class="col-sm-8">
+                                    <p class="modal-name"></p>
+                                    <input class="modal-name" type="hidden" name="name" value="">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <p class="col-sm-4 col-form-label">メールアドレス<span class="badge badge-danger ml-1">必須</span></p>
+                                <div class="col-sm-8">
+                                    <p class="modal-email"></p>
+                                    <input class="modal-email" type="hidden" name="email" value="">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <p class="col-sm-4 col-form-label">電話番号</p>
+                                <div class="col-sm-8">
+                                    <p class="modal-tel"></p>
+                                    <input class="modal-tel" type="hidden" name="tel" value="">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <p class="col-sm-4 col-form-label">性別<span class="badge badge-danger ml-1">必須</span></p>
+                                <div class="col-sm-8">
+                                    <p class="modal-gender"></p>
+                                    <input class="modal-gender" type="hidden" name="gender" value="">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <p class="col-sm-4 col-form-label">選択（複数選択可）<span class="badge badge-danger ml-1">必須</span></p>
+                                <div class="col-sm-8">
+                                    <p class="modal-checkbox"></p>
+                                    <input class="modal-checkbox" type="hidden" name="checkbox" value="">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <p class="col-sm-4 col-form-label">お問い合わせ内容<span class="badge badge-danger ml-1">必須</span></p>
+                                <div class="col-sm-8">
+                                    <p class="modal-contents"></p>
+                                    <input class="modal-contents" type="hidden" name="contents" value="">
+                                </div>
+                            </div>
+
+                            <div class="text-center">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">修正する</button>
+                                {{ Form::submit('送信', ['name' => 'submit', 'class' => 'btn btn-primary']) }}
+                            </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script src="{{ asset('/js/modal.js') }}"></script>
 </body>
 </html>
 
