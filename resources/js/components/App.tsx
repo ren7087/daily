ab06607e47342a67d98775bf97cd43f69b717792
User <user@MacBook-Air.local>
2022-07-09 15:05:43 +0900

    fullcalendarイベント追加を可能にする

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -1,54 +1,66 @@
 import ReactDOM from 'react-dom';
-import FullCalendar from "@fullcalendar/react";
+import { useCallback, useState } from "react";
+import FullCalendar, {
+  DateSelectArg,
+  EventApi,
+  EventClickArg
+} from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin, {DateClickArg} from '@fullcalendar/interaction';
-import allLocals from "@fullcalendar/core/locales-all";
-import { useCallback } from 'react';
-
-const thisMonth = () => {
-    const today = new Date();
-    return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
-      2,
-      "0"
-    )}`;
-};
+import allLocales from "@fullcalendar/core/locales-all";
+import interactionPlugin from "@fullcalendar/interaction";
+import { INITIAL_EVENTS, createEventId } from "./event-utils";
 
 function App() {
-    const handleDateClick = useCallback((arg: DateClickArg) => {
-        alert(arg.dateStr);
-    }, []);
-    const todayStr = new Date().toISOString().replace(/T.*$/, "");
-    return (
-        <div>
+  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
+  const handleEvents = useCallback(
+    (events: EventApi[]) => setCurrentEvents(events),
+    []
+  );
+  const handleDateSelect = useCallback((selectInfo: DateSelectArg) => {
+    let title = prompt("イベントのタイトルを入力してください")?.trim();
+    let calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect();
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }, []);
+  const handleEventClick = useCallback((clickInfo: EventClickArg) => {
+    if (
+      window.confirm(`このイベント「${clickInfo.event.title}」を削除しますか`)
+    ) {
+      clickInfo.event.remove();
+    }
+  }, []);
+  return (
+    <div className="demo-app">
+      <div className="demo-app-main">
         <FullCalendar
-            initialView="dayGridMonth"
-            locales={allLocals}
-            locale="ja"
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-                start: "prev,next today",
-                center: "title",
-                end: "dayGridMonth,timeGridWeek,timeGridDay",
-            }}
-            events={[
-                { title: "event 1", date: `${thisMonth()}-01` },
-                { title: "event 2", date: `${thisMonth()}-02` },
-                {
-                    title: "All-day event",
-                    start: todayStr,
-                },
-                {
-                    title: "Timed event",
-                    start: todayStr + "T12:00:00",  // 時刻はTで結ぶ
-                },
-            ]}
-            dateClick = {handleDateClick}
-            selectable={true}
-            editable={true}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            start: "prev,next today",
+            center: "title",
+            end: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          initialView="dayGridMonth"
+          selectable={true}
+          editable={true}
+          initialEvents={INITIAL_EVENTS}
+          locales={allLocales}
+          locale="ja"
+          eventsSet={handleEvents}
+          select={handleDateSelect}
+          eventClick={handleEventClick}
         />
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
